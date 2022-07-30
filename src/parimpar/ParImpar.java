16829c29a645c317361dca608808faee5646cc66
Wellingt0ndev <wellington_006@hotmail.com>
2022-07-30 11:40:33 -0300

    Add: comentário

--- src/parimpar/ParImpar.java
+++ src/parimpar/ParImpar.java
@@ -1,6 +1,16 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * * Question 1
+*Hopper e Joyce precisam fechar o portão do mundo invertido, que está conectado
+*a uma máquina abaixo do Shopping de Starcourt. Esse portão possui um segredo e 
+*para fechá-lo é preciso responder a seguinte pergunta:
+*Qual é a diferença entre a soma dos números ímpares e a soma dos números pares
+*de 1 a 1985?
+*
+*1+3+5+7+9+...+1983 + 1985 - (2+4+6+8...+1982+1984)
+*
+*Desenvolva um código para que sua saída seja o resultado.
+*Você pode usar sua IDE de preferência,
+*mas não esqueça de colocar o código abaixo.
  */
 package parimpar;
 
